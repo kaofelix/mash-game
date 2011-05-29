3ab4722fdbd0066696c901cdb9e5b5fcac4730a8
Kao Félix <kcfelix@gmail.com>
2011-05-29 05:26:43 -0300

    Moving the character around.

--- mash/client.py
+++ mash/client.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pyglet
+from pyglet.window import key
+from vec2 import Vec2
 
 class Player(object):
+    speed = 75
+
     def __init__(self, name, position):
         image = pyglet.image.load("../data/blueplayer.png")
         image.anchor_x = image.width // 2
@@ -30,15 +34,34 @@ class Player(object):
         self.label.x = x
         self.label.y = y + self.sprite.height//2
 
+    def move(self, direction, dt):
+        self.position = Vec2(self.position) + direction * self.speed * dt
+
 window = pyglet.window.Window(800, 600)
-player = Player("Name", (100, 100))
+key_state = key.KeyStateHandler()
+window.push_handlers(key_state)
+player = Player("Name", (150, 150))
 
 @window.event
 def on_draw():
     window.clear()
     player.draw()
 
+def update(dt):
+    x, y = 0, 0
+    if key_state[key.W]:
+        y += 1
+    if key_state[key.S]:
+        y -=1
+    if key_state[key.D]:
+        x += 1
+    if key_state[key.A]:
+        x -= 1
+    direction = Vec2(x,y).normalize()
+    player.move(direction, dt)
+
 def main():
+    pyglet.clock.schedule_interval(update, 1.0/60.0)
     pyglet.app.run()
 
 if __name__ == '__main__':
